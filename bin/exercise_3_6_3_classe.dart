484410ceec4facb90e415ce5b4dc2872fcd42d3b
Viktor <komvik.slack@gmail.com>
2024-09-04 10:28:23 +0200

    Start Programm

---
+++ bin/exercise_3_6_3_classe.dart
@@ -0,0 +1,5 @@
+// import 'package:exercise_3_6_3_classe/exercise_3_6_3_classe.dart' as exercise_3_6_3_classe;
+
+void main() {
+  print('Start__________________________Project 3.6.3');
+}
---
+++ test/exercise_3_6_3_classe_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercise_3_6_3_classe/exercise_3_6_3_classe.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
